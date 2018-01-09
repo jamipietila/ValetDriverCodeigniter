be6a6c11adaa03bfe380b6a9e7d9e2ede66c4271
Jammyman <jami.pietila@gmail.com>
2018-01-09 21:45:52 +0200

    first commit

---
+++ CodeigniterValetDriver.php
@@ -0,0 +1,52 @@
+<?php
+
+class CodeigniterValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        if (file_exists($sitePath.'/system/core/CodeIgniter.php')) {
+             return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+
+	if (file_exists($staticFilePath = $sitePath.'/public_html/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        return $sitePath.'/public_html/index.php';
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+#Laravel Valet Driver for Codeigniter
+
+Tested with OS X and Valet 2.0.7.
+
+## Info
+Driver is expecting that public folder is called public_html.
+
+## Installation OS X
+Copy file to ~/.valet/Drivers folder.
+
